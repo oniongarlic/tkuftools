86b79e4c643e9c7bd2bb2553ec57700c15a51762
Kaj-Michael Lang <milang@tal.org>
2018-06-19 10:07:38 +0300

    Make sure of requested types. Dump more data in debug dumper.

--- json.c
+++ json.c
@@ -7,6 +7,7 @@ const char *json_get_string(json_object *o, const char *key)
 json_object *tmp;
 
 if (json_object_object_get_ex(o, key, &tmp))
+    if (json_object_is_type(o, json_type_string))
 	return json_object_get_string(tmp);
 
 return NULL;
@@ -17,7 +18,8 @@ int json_get_int(json_object *o, const char *key, int defval)
 json_object *tmp;
 
 if (json_object_object_get_ex(o, key, &tmp))
-	return json_object_get_int(tmp);
+    if (json_object_is_type(o, json_type_int))
+    	return json_object_get_int(tmp);
 
 return defval;
 }
@@ -27,7 +29,8 @@ array_list* json_get_array(json_object *o, const char *key)
 json_object *tmp;
 
 if (json_object_object_get_ex(o, key, &tmp))
-	return json_object_get_array(tmp);
+    if (json_object_is_type(o, json_type_array))
+    	return json_object_get_array(tmp);
 
 return NULL;
 }
@@ -54,18 +57,45 @@ void json_dump_type(int type)
 
 void json_dump(json_object *o)
 {
-int type = json_object_get_type(o);
-printf("type of object: [%d] ", type);
+int type=json_object_get_type(o);
+printf("\nJSON Object: [%d], ", type);
 json_dump_type(type);
 
-if (type==json_type_object) {
-    json_object_object_foreach(o, key, val) {
-    	type = json_object_get_type(val);
-	    printf("key: \"%s\", type of val: [%d] ", key, type);
-        json_dump_type(type);
+switch (type) {
+    case json_type_string:
+        printf("String: %s\n", json_object_get_string(o));
+    break;
+    case json_type_int:
+        printf("Int: %d\n", json_object_get_int(o));
+    break;
+    case json_type_double:
+        printf("Double: %f\n", json_object_get_double(o));
+    break;
+    case json_type_boolean:
+        printf("Bool: %d\n", json_object_get_boolean(o));
+    break;
+    case json_type_object: 
+    {
+        printf("Object: fields: %d\n", json_object_object_length(o));
+        json_object_object_foreach(o, key, val) {
+        	type = json_object_get_type(val);
+	        printf("key: \"%s\", contents:\n", key);
+            json_dump(val);
+        }
+        printf("--\n");
     }
-} else if (type==json_type_array) {
-    int l=json_object_array_length(o);
-    printf("items: %d ", l);
-}
+    break;
+    case json_type_array:
+    {
+        int l=json_object_array_length(o);
+        printf("Array: items: %d\n", l);
+        for (int x=0;x<l;x++  ) {
+            json_object *a=json_object_array_get_idx(o, x);
+            json_dump(a);
+        }
+        printf("--\n");
+    }
+    break;
+    default:;
+}    
 }
